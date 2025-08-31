9ccc08a11faf0c0b9dc013e2115736c6a18cd8cf
Mobin Solahaei <solahaei.2@gmail.com>
2025-08-31 16:35:17 +0330

    fix: final vite build setup for Render

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: "dist"
   },
   server: {
     port: 5173,
-    host: '0.0.0.0'
+    host: "0.0.0.0"
   }
 });

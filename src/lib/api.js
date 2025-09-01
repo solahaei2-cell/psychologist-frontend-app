3a21ad9680b293fc9d38dbc6c8749dccd61f4db0
Mobin Solahaei <solahaei.2@gmail.com>
2025-09-01 18:29:56 +0330

    Fix API URL

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL  'httplocalhost5000';
+export const API_URL = 'https://psychologist-ai-fhcp.onrender.com';
+
 
 const api = axios.create({
   baseURL API_BASE,

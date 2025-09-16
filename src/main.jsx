3ab3554acc8e14dd04400a2111aacb13a39ad15a
Mobin Solahaei <solahaei.2@gmail.com>
2025-09-16 16:25:02 +0330

    chore(axios): set Authorization header from storage at app boot

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
+import axios from 'axios'
 import App from './App'
 import './index.css';
+
+// Set axios Authorization header from storage on app boot
+try {
+  const bootToken = localStorage.getItem('token') || sessionStorage.getItem('token');
+  if (bootToken) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${bootToken}`;
+    // eslint-disable-next-line no-console
+    console.debug('[main] Boot token present, Authorization header set');
+  }
+} catch {}
 
 
 createRoot(document.getElementById('root')).render(
